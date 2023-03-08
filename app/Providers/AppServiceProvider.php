1fcf7c9f27565c3e05f7841d922ec6cb1e1238fe
Nanako-I <milklmi.1103775@gmail.com>
2023-03-08 13:07:27 +0000

    html追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;    //この行を追加
+use Illuminate\Support\Facades\URL;    //この行を追加しなさい
 
 class AppServiceProvider extends ServiceProvider
 {
